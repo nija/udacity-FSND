c698812fa1c5dbe039147aba979dc9b577329c06
Nija Mashruwala <nija.mashruwala@gmail.com>
2015-11-14 12:35:00 -0800

    initial commit of files

---
+++ project1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""This file is the main FSND project source
+
+Example:
+    Call the program by::
+
+        $ ./project1.py
+    It will launch a browser tab with 6 movie tiles which you can click on to
+    get to the movie trailer
+"""
+
+from media import MovieMetadata
+from fresh_tomatos import open_movies_page
+
+def create_movielist():
+    """Pylint complained, so I moved this into its own function
+
+    Example:
+        This returns a populated list of Movie_metadata objects::
+
+            create_movielist()
+    """
+    # Create the list of movies - let's pick 6
+    movielist = []
+    # title, box_art, url
+    movielist.append(MovieMetadata("Toy Story", \
+        "I'm from Mattel. Well, I'm not really from Mattel, I'm actually from a smaller company that was purchased by Mattel in a leveraged buyout.", \
+        'http://ia.media-imdb.com/images/M/MV5BMTgwMjI4MzU5N15BMl5BanBnXkFtZTcwMTMyNTk3OA@@._V1_SY317_CR12,0,214,317_AL_.jpg', \
+        'https://www.youtube.com/watch?v=KYz2wyBy3kc'))
+    movielist.append(MovieMetadata("Avatar", \
+        "I was hoping for some kind of tactical plan that didn't involve martyrdom", \
+        'http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SY317_CR0,0,214,317_AL_.jpg', \
+        'https://www.youtube.com/watch?v=cRdxXPV9GNQ'))
+    movielist.append(MovieMetadata("The Princess Bride", \
+        "When I was your age, television was called books. And this is a special book. It was the book my father used to read to me when I was sick, and I used to read it to your father. And today I'm gonna read it to you.", \
+        'http://ia.media-imdb.com/images/M/MV5BMTkzMDgyNjQwM15BMl5BanBnXkFtZTgwNTg2Mjc1MDE@._V1_SY317_CR0,0,214,317_AL_.jpg', \
+        'https://www.youtube.com/watch?v=GNvy61LOqY0'))
+    movielist.append(MovieMetadata("Serenity", \
+        "Shiny. Let's be bad guys.", \
+        'http://ia.media-imdb.com/images/M/MV5BMTI0NTY1MzY4NV5BMl5BanBnXkFtZTcwNTczODAzMQ@@._V1_SY317_CR0,0,214,317_AL_.jpg', \
+        'https://www.youtube.com/watch?v=JY3u7bB7dZk'))
+    movielist.append(MovieMetadata("The Wizard of Speed and Time", \
+        "Miss Belair, if you feel compelled to grab part of my body and shake it before you can even be friendly, you've got far worse problems than you think I have.", \
+        'http://ia.media-imdb.com/images/M/MV5BODc3MzA3MDQyN15BMl5BanBnXkFtZTYwMzE2MTk5._V1_SX214_AL_.jpg', \
+        'https://www.youtube.com/watch?v=3ldOTw60Ozg'))
+    movielist.append(MovieMetadata("Inside Out", \
+        "Take her to the moon for me. Okay?", \
+        'http://ia.media-imdb.com/images/M/MV5BOTgxMDQwMDk0OF5BMl5BanBnXkFtZTgwNjU5OTg2NDE@._V1_SX214_AL_.jpg', \
+        'https://www.youtube.com/watch?v=yRUAzGQ3nSY'))
+
+    return movielist
+
+# Call the function to open the webpage
+open_movies_page(create_movielist())
---
+++ tests_for_project1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""Nose2 tests for project1.py, media.py, and fresh_tomatos.py
+    This is more to get me used to using nose2 and pylint than useful tests"""
+
+from media import MovieMetadata
+from fresh_tomatos import create_movie_tiles_content
+
+
+def create_movielist():
+    """Function for testing lists - populates a list of movies with sample data"""
+    movielist = []
+    for x in range(5):
+        movielist.append(MovieMetadata("Movie Title" + (x+1).__str__(), \
+            "Movie comment" + (x+1).__str__(), \
+            "http://poster_image_url.png", \
+            "http://trailer_youtube.url"))
+    return movielist
+
+def test_check_movielist():
+    """Tests a list of movies"""
+    movielist = create_movielist()
+    assert len(movielist) == 5
+
+def test_object_str_output():
+    """Tests the string output of the MovieMetadata object"""
+    a = MovieMetadata("Movie Title", "Movie comment", "http://box_art.png", "http://youtube.url")
+    print "test_object_str_output:\n"
+    print "created a"
+    print a
+    assert a.__str__() == "MovieMetadata object with state: title:Movie Title comment:Movie comment poster_image_url:http://box_art.png trailer_youtube_url:http://youtube.url"
+
+def test_object_repr_output():
+    """TODO: Need to figure out how to mock-up some number of digits regex for this test.
+        Until then, this is commented out
+
+        Tests the object representation of the MovieMetadata object"""
+#   a = MovieMetadata("Movie Title", "Movie comment", http://box_art.png", "https://youtube.url")
+#   print "created a"
+#   print "%s" % (a.__repr__())
+#   assert a.__repr__() == "MovieMetadata 4325376592 < title:Movie Title comment:Movie comment poster_image_url:http://box_art.png trailer_youtube_url:http://youtube.url >"
+    pass
+
+def test_create_movie_tiles_content():
+    """Tests the content creation for the movie tiles"""
+    movielist = create_movielist()
+    content = create_movie_tiles_content(movielist)
+    print "test_create_movie_tiles_content: \n"
+    print content
+    assert content != ''
